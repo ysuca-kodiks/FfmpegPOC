34004254a0b8d448e57e33fd0e853bd9a33ab36b
Fatih Altuntaş <36915172+fatihaltuntas@users.noreply.github.com>
2022-01-27 13:03:33 +0300

    video parçalanması,son ts üzerinden video kontrolü ve klasör içine video atma işi bitti.

--- FfmpegPOC/Program.cs
+++ FfmpegPOC/Program.cs
@@ -5,16 +5,18 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Threading;
 using Hangfire;
+using System.Linq;
 
 namespace FfmpegPOC
 {
     class Program
     {
+        public static Process ffmpegProcess;
         static void Main(string[] args)
         {
 
             string wPath = @"C:\ffmpeg\records";
-            string lPath = Path.Combine(Directory.GetCurrentDirectory(),"records");
+            string lPath = Path.Combine(Directory.GetCurrentDirectory(), "records");
 
             Console.WriteLine("VideoId giriniz");
             string videoId = Console.ReadLine();
@@ -22,38 +24,44 @@ namespace FfmpegPOC
             Console.WriteLine("IsLive Y/N");
             string isLive = Console.ReadLine();
 
-
-
             switch (isLive)
             {
                 case "y":
-                    LiveScenario(videoId,wPath);
+
+                    LiveScenario(videoId, wPath, true);
                     break;
 
                 default:
                     OfflineScenario(videoId);
                     break;
             }
-
-
             Console.ReadLine();
+        }
 
+        public static void OnExited(object sender,EventArgs e)
+        {
+            
+        }
+        public static void OnDisposed(object sender, EventArgs e)
+        {
 
         }
 
-        private static void LiveScenario(string videoId,string wPath)
+        private static void LiveScenario(string videoId, string wPath, bool isVideoCut)
         {
             Console.WriteLine("Merhaba Live");
 
             string link = '"' + "https://www.youtube.com/watch?v=" + videoId + '"';
             string streamLinkCmd = "/C streamlink " + link + " --stream-url";
             string streamLink = string.Empty;
-
+            DirectoryInfo directory;
+            var path = wPath + "/" + videoId;
+            
             Console.WriteLine(streamLinkCmd);
             var streamLinkStartInfo = new ProcessStartInfo
             {
                 //FileName = "/usr/local/bin/streamlink",
-                FileName="cmd.exe",
+                FileName = "cmd.exe",
                 Arguments = streamLinkCmd,
                 UseShellExecute = false,
                 RedirectStandardOutput = true
@@ -68,53 +76,78 @@ namespace FfmpegPOC
                 process.Dispose();
             }
 
-            string ffmpegCmd = "/C ffmpeg -y -i t=1" + '"' + streamLink.TrimEnd() + '"' + " -hls_time 9 -hls_segment_filename " + '"' + "index-%d.ts" + '"' + " -hls_playlist_type vod index.m3u8";
+            string ffmpegCmd = "ffmpeg -y -i " + '"' + streamLink.TrimEnd() + '"' + " -hls_time 9 -hls_segment_filename " + '"' + "index-%d.ts" + '"' + " -hls_playlist_type vod index.m3u8";
+
+            if (isVideoCut)
+                ffmpegCmd += " -t 120";
+
             Console.WriteLine(ffmpegCmd);
+
+            
+            if (!Directory.Exists(path))
+                directory = Directory.CreateDirectory(path);
 
             var ffmpegStartInfo = new ProcessStartInfo
             {
                 //FileName = "/bin/bash",
-                FileName="cmd.exe",
-                Arguments = ffmpegCmd,
-                WorkingDirectory = wPath,
-                UseShellExecute = false,
-                
-
+                FileName = "cmd.exe",
+                Arguments = @"/C " + ffmpegCmd,
+                WorkingDirectory = path,
+                UseShellExecute = false
             };
 
-            using (var ffmpegProcess = new Process { StartInfo = ffmpegStartInfo })
+            try
             {
-                var firstDt = DateTime.Now;
-                ffmpegProcess.Start();
-
-                
-
-                Console.WriteLine("ProcessorName: " + ffmpegProcess.ProcessName);
-                //Task.Delay(TimeSpan.);
-
-                //ffmpegProcess.Kill(true);
-
-                ffmpegProcess.WaitForExit();
-
-                if (ffmpegProcess.HasExited)
+                using (ffmpegProcess = new Process { StartInfo = ffmpegStartInfo })
                 {
-                    Console.WriteLine("StartTime: " + ffmpegProcess.StartTime);
-                    Console.WriteLine("ProcessorTime: " + ffmpegProcess.TotalProcessorTime.TotalMinutes);
-                    ffmpegProcess.Kill(true);
+                    ffmpegProcess.Exited += OnExited;
+                    ffmpegProcess.Disposed += OnDisposed;
+                    var firstDt = DateTime.Now;
+
+                    ffmpegProcess.Start();
+                    Console.WriteLine("ProcessorName: " + ffmpegProcess.ProcessName);
+
+
+                    var timer = new System.Threading.Timer((e) =>
+                    {
+                        DirectoryInfo info = new DirectoryInfo(wPath);
+                        FileInfo file = info.GetFiles().Where(x => x.FullName.Contains(".ts")).OrderByDescending(p => p.CreationTime).FirstOrDefault();
+                        if (file != null)
+                        {
+                            if (file.CreationTime.AddMinutes(1) < DateTime.Now)
+                            {
+                                //Docker üzerinden container kaldırılacak.
+                                Console.WriteLine("StartTime: " + ffmpegProcess.StartTime);
+                                Console.WriteLine("ProcessorTime: " + ffmpegProcess.TotalProcessorTime.TotalMinutes);
+                                ffmpegProcess.Kill(true);
+                            }
+                        }
+
+                    }, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+                    ffmpegProcess.WaitForExit();
+
+
+                    if (ffmpegProcess.HasExited)
+                    {
+                        Console.WriteLine("StartTime: " + ffmpegProcess.StartTime);
+                        Console.WriteLine("ProcessorTime: " + ffmpegProcess.TotalProcessorTime.TotalMinutes);
+                        ffmpegProcess.Kill(true);
+                    }
                 }
             }
-
-
+            catch (Exception ex)
+            {
+                Environment.Exit(0);
+            }
         }
-        
         // TODO:@ucaselimyavuz 
         private static void OfflineScenario(string videoId) { }
+        private static void CheckLiveVideo()
+        {
 
+        }
     }
-
-
-
-
 }
 
 
